f8a0f6146de7e78c542eed1ab1121ea1d9120cc3
Dyony <dyony_rezende@live.com>
2020-08-12 13:56:19 -0300

    web project practice

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var conn = require('./../inc/db');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  conn.query(`
+      SELECT * FROM tb_menus ORDER BY title
+  `, (err, results)=>{
+
+    if(err){
+
+      console.log(err);
+
+    }
+
+
+  res.render('index', { title: 'Restaurante Saboroso' ,
+                        menus: results
+    });
+
+  });
+
+});
+
+router.get('/contacts', function(req, res, next){
+
+  res.render('contact', { title: 'Contato - Restaurante Saboroso' });
+
+});
+
+router.get('/menus', function(req, res, next){
+
+  res.render('menu', { title: 'Menu - Restaurante Saboroso' });
+
+});
+
+router.get('/reservations', function(req, res, next){
+
+  res.render('reservation', { title: 'Reserva - Restaurante Saboroso' });
+
+});
+
+router.get('/services', function(req, res, next){
+
+  res.render('services', { title: 'Serviços - Restaurante Saboroso' });
+
+});
+
+
+module.exports = router;
